bdcf98100cf18ccf6406110204b6655abc04b47b
Tomasz Dworowy <dworowytomasz@gmail.com>
2024-07-21 11:06:20 +0200

    update

--- helloworld_lkm/helloworld_lkm.c
+++ helloworld_lkm/helloworld_lkm.c
@@ -1,19 +1,19 @@
 #include <linux/init.h>
 #include <linux/module.h>
 
-MODULE_AUTHOR("null");
-MODULE_DESCRIPTION("LKP book:ch4/helloworld_lkm: hello, world, our first LKM");
+MODULE_AUTHOR("<insert your name here>");
+MODULE_DESCRIPTION("LKP2E book:ch4/helloworld_lkm: hello, world, our first LKM");
 MODULE_LICENSE("Dual MIT/GPL");
-MODULE_VERSION("0.1");
+MODULE_VERSION("0.2");
 
-static int __init helloworld_lkm_init(void) {
-    printk(KERN_INFO "Hello, World\n";
-    return 0;
+static int __init helloworld_lkm_init(void)
+{
+	printk(KERN_INFO "Hello, world\n");
+	return 0;		/* success */
 }
-
-static void __exit helloworld_lkm_exit(void) {
-    printk(KERN_INFO "Goodbye, World\n");
+static void __exit helloworld_lkm_exit(void)
+{
+	printk(KERN_INFO "Goodbye, world! Climate change has done us in...\n");
 }
-
 module_init(helloworld_lkm_init);
 module_exit(helloworld_lkm_exit);
